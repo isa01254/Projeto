e3905ba893e19c49011abd9effbb82f8198cdb04
Isadora Amaral Pereira <15157372680@MUZAMBINHO.EDU.BR>
2026-06-08 09:48:01 -0300

    atualização

--- config/settings.py
+++ config/settings.py
@@ -57,8 +57,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projeto',
-        'USER': 'projeto_user',
+        'NAME': 'Projeto',
+        'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
